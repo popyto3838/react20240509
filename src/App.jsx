5c09782456e131a63978a8a5ef3aa5b4b2698e10
Hongbin Yoon <popyto@naver.com>
2024-05-09 14:31:44 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
 function App() {
+  // &&, ||
+
+  // falsy(false 같은)
+  // : false, 0,"", null undefined , NaN
+
+  const a = "";
+
+  if (a) {
+    console.log("a는 참이다");
+  } else {
+    console.log("a는 거짓이다");
+  }
+
+  // truthy(true 같은): falsy 가 아니면 truthy
+
+  // && , || 와 조합
+  let b = 0;
+
+  let c = b && "값이 있음";
+  console.log("c", c);
+
+  let d = b || " 값이 없음";
+  console.log("d", d);
+
   return <div></div>;
 }
 
