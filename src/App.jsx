181e18c465ad2eaae175782a092e64f499767acc
Hongbin Yoon <popyto@naver.com>
2024-05-09 14:42:39 +0900

    자바스크립트 데이터타입

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 function App() {
-  // &&, ||
+  // javascript review
 
-  // falsy(false 같은)
-  // : false, 0,"", null undefined , NaN
+  // javascript data type
+  // 값을적지않으면 undefined
+  let a; //값이 할당되지 않으면 undefined
+  console.log("a", typeof a);
 
-  const a = "";
+  let b = "문자열 타입"; // string
+  console.log("b", typeof b);
 
-  if (a) {
-    console.log("a는 참이다");
-  } else {
-    console.log("a는 거짓이다");
-  }
+  let c = 3; //number 정수실수 안나누고 number
+  console.log("c", typeof c);
 
-  // truthy(true 같은): falsy 가 아니면 truthy
+  let e = true; // boolean
+  console.log("e", typeof e);
 
-  // && , || 와 조합
-  let b = 0;
+  let f = {}; // object  객체 타입
+  console.log("f", typeof f);
 
-  let c = b && "값이 있음";
-  console.log("c", c);
+  let g = function () {}; // function 타입
+  console.log("g", typeof g);
 
-  let d = b || " 값이 없음";
-  console.log("d", d);
+  g = {};
+  console.log("g", typeof g);
+
+  // 자바스크립트에서는 변수에 값이 들어갈떄 타입이 결정된다
 
   return <div></div>;
 }
