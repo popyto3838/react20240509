7cee56d14144bac606d494d1e2f69ca4b6ea71d3
Hongbin Yoon <popyto@naver.com>
2024-05-09 12:27:42 +0900

    자바스크립트값연습하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return;
+  const a = 5;
+  const b = 9;
+
+  function someMethod() {
+    return "hello js";
+  }
+  function someMethod2(a) {
+    <p>a +""+a</p>;
+  }
+
+  return (
+    <>
+      <div>
+        <p>
+          {a}x{b} ={a * b}
+        </p>
+
+        <p>{someMethod()}</p>
+        <p>{someMethod2("react")}</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,5 +1,27 @@
 function App() {
-  return <div>hello react</div>;
+  const a = 5;
+  const b = 9;
+
+  function someMethod() {
+    return "hello js";
+  }
+
+  function someMethod2(param) {
+    return param + " " + param;
+  }
+
+  return (
+    <>
+      <div>
+        {/* <p> 5 X 9 = 45 </p> */}
+        <p>
+          {a} X {b} = {a * b}
+        </p>
+        <p>{someMethod()}</p> {/* hello js */}
+        <p>{someMethod2("react")}</p> {/* react react */}
+      </div>
+    </>
+  );
 }
 
 export default App;
