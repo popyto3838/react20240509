82660914e7e0740a690260d7e2a159649b148c82
Hongbin Yoon <popyto@naver.com>
2024-05-09 14:15:47 +0900

    자바스크립트값연습하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 function App() {
   const name = "강인";
-  const position = "미드필더";
-  const salary = 30;
+  const age = 5; // or 30;
 
   return (
     <div>
-      <p>
-        {position} {name}의 연봉은 {salary}억입니다
-      </p>
+      <p>{name}</p>
+      {name == "흥민" && <p>공격수</p>}
+      {name == "강인" && <p>미드필더</p>}
+      {/*  age가 20보다 크면
+   <p>투표 가능 </p>
+   age가 20보다 작으면
+   <p> 투표 불가능 </p>
+   */}
+      {age >= 20 && <p>투표가능</p>}
+      {age < 20 && <p>투표불가능</p>}
     </div>
   );
 }
