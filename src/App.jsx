0016829f3887a86a8df7e1ab271aba618fd99844
Hongbin Yoon <popyto@naver.com>
2024-05-09 15:22:12 +0900

    object와 property

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
+let son = {
+  firstName: "흥민",
+  lastName: "손",
+  address: "이대",
+  country: "한국",
+  married: true,
+};
+
 function App(props) {
-  const obj = {
-    age: 30,
-    name: "John Doe",
-    email: "john@example.com",
-    password: "1234567890",
-  };
   return (
     <div>
       <ul>
-        <li>age;{obj.age}</li>
-        <li>name;{obj.name}</li>
-        <li>email;{obj.email}</li>
-        <li>password;{obj.password}</li>
+        <li>성:{son.firstName}</li>
+        <li>이름:{son.lastName}</li>
+        <li>주소:{son.address}</li>
+        <li>국가:{son.country}</li>
+        <li>결혼여부:{son.married ? "기혼" : "미혼"}</li>
       </ul>
     </div>
   );
