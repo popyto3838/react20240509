c0464cb7e30a918dbbee5349c40f70c2fc154b7b
Hongbin Yoon <popyto@naver.com>
2024-05-09 12:16:51 +0900

    자바스크립트값연습하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 function App() {
-  const name = "강인";
-  const position = "미드필더";
-  const salary = 30;
-
-  return (
-    <div>
-      <p>
-        {position} {name}의 연봉은 {salary}억입니다
-      </p>
-    </div>
-  );
+  return;
 }
 
 export default App;
