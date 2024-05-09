cea5f6981a85edc262e9755ad90fed19ab4c003c
Hongbin Yoon <popyto@naver.com>
2024-05-09 15:37:49 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 
 function App(props) {
-  //object
+  const a = {};
+  console.log("a", typeof a);
 
-  const a = {
-    name: "john",
-  };
-  const b = a;
+  const b = []; // 배열
+  console.log("b", typeof b);
 
-  console.log("a.name", a.name); //john
-  console.log("b.name", b.name); //john
+  //배열 : 여러 값을 순서(index)대로 가지고 있는 객체이다
 
-  b.name = "jane";
-  console.log("a.name", a.name); //jane
-  console.log("b.name", b.name); //jane
+  const c = ["son", "lee", "kim", "hwang"];
+  console.log("c", c);
+  console.log("c의 0", c[0]);
+  console.log("c의 1", c[1]);
+  console.log("c의 2", c[2]);
+  console.log("c의 3", c[3]);
+  console.log("c의 4", c[4]); //undefined
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>{c[0]}</li>
+          <li>{c[1]}</li>
+          <li>{c[2]}</li>
+          <li>{c[3]}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
