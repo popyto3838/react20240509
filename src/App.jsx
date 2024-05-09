f033969055f420e6f83022da446251c555079956
Hongbin Yoon <popyto@naver.com>
2024-05-09 10:47:16 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
+//built-in component : html 의 기본 요소, 태그명 소문자로 시작
+//component : 직접만든 요소(태그), 대문
 
-import './App.css'
+//component 는 return을 가진 함수
+
+function Mycomponent() {
+  // return : 화면에 출력할 코드
+  return <div>hello component</div>;
+}
+
+function MyComp() {
+  //jsx 코드 : <div></div>
+}
 
 function App() {
+  //한줄 주석
+  /*
+  여러줄 주석
+  */
 
+  //component 사용시 종료 태그 생략할 수 없음
+  //br 같은 빈 요소는 시작 태그에서 꼭 종료
 
   return (
     <>
       <div>
-          <h1>hello react</h1>
+        <h1>
+          hello
+          <br />
+          react
+        </h1>
+        <h1>하이 리액트</h1>
+        <h1>안녕 리액트</h1>
+        <p>lorem ipsum dolor.</p>
+        <Mycomponent />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
