edc90601b43e3b93e67b4836730c44b9417c667a
Hongbin Yoon <popyto@naver.com>
2024-05-09 16:23:44 +0900

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const foodList = ["pizza", "burger", "noodle", "coffee"];
+
+  return (
+    <div>
+      <ul>
+        <li>
+          {foodList.map((food) => (
+            <li>{food}</li>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
