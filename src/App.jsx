38244e81a9112fa01c2cdffaea389ba2f2947999
Hongbin Yoon <popyto@naver.com>
2024-05-09 11:39:07 +0900

    필요 없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-function MyComponent() {
-  // 최상위 컴포넌트 사용하지 않을 때 ->fragment <></> 사용
+function App() {
+  // const, let 중에 사용
+  const myTag = <h1>hello</h1>;
+  const val1 = "흥민";
+  const val2 = 3.14;
+  const val3 = 456;
+  const val4 = true;
+  const val5 = false;
+  const val6 = null;
+  const val7 = undefined;
+  const val8 = [2, 3];
+  const val9 = { age: 3, name: "강인" };
+
+  // {} : jsx 내에서 javascript 코드 사용
+
   return (
     <>
-      <p>Lorem ipsum dolor.</p>
-      <p>Aut mollitia, temporibus?</p>
+      <p>
+        {val1} 선수는 연봉이{val3}억원 이다.
+      </p>
+      <div>{val1}</div>
+      <div>{val2}</div>
+      <div>{val3}</div>
+      <div>{myTag}</div>
+      {/* boolean, null, undefined 은 출력 안됨 */}
+      <div>{val4}</div>
+      <div>{val5}</div>
+      <div>{val6}</div>
+      <div>{val7}</div>
+
+      <div>{val8}</div>
+
+      {/* 객체는 오류 */}
+      {/*<div>{val9}</div>*/}
+      {/* 프로퍼티에 접근해서 사용 */}
+      <div>{val9.name}</div>
+      <div>{val9.age}</div>
     </>
-  );
-}
-
-function MyComp() {
-  // 최상위 컴포넌트는 1개임
-  return (
-    <div>
-      <p>Lorem ipsum.</p>
-      <p>Delectus, voluptatibus!</p>
-    </div>
-  );
-}
-
-function MyTag() {
-  // jsx 코드 가 여러줄일때는 () 사용 div 안에 감싸기
-  return (
-    <div>
-      hello tag
-      <p>Lorem ipsum</p>
-      <p>Est,quos?</p>
-    </div>
-  );
-}
-function App() {
-  return (
-    <div>
-      <MyTag />
-      <MyComp />
-      <MyComponent />
-    </div>
   );
 }
 
