b6e08c433c26747c2cd967037a2ef728a7764710
Hongbin Yoon <popyto@naver.com>
2024-05-09 16:24:48 +0900

    props 활용연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-function App(props) {
-  const foodList = ["pizza", "burger", "noodle", "coffee"];
-
+function MyButton({ name, onClick }) {
   return (
     <div>
-      <ul>
-        <li>
-          {foodList.map((food) => (
-            <li>{food}</li>
-          ))}
-        </li>
-      </ul>
+      <button onClick={onClick}>{name}</button>
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyButton name="샾 출력" onClick={() => console.log("####")} />
+      <MyButton name="느낌표 출력" onClick={() => console.log("!!!!")} />
     </div>
   );
 }
