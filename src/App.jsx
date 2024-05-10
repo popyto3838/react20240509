e9eb381914a7eae1a26c5e78131cae5c1323883b
Hongbin Yoon <popyto@naver.com>
2024-05-10 15:02:35 +0900

    props의 값이 없을때 기본값 할당하는 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+function MyButton({ name = "클릭" }) {
+  return <button>{name}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton name="버튼1" />
+      <MyButton />
+    </div>
+  );
 }
 
 export default App;
