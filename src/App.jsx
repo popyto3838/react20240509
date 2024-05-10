0141b6a72ee13394921fa901f91d3d39b0feb263
Hongbin Yoon <popyto@naver.com>
2024-05-10 14:14:46 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
-//<MyButton name="클릭3"/>
-// props :{name:"클릭"}
-
-function MyButton(props) {
-  return (
-    <button
-      style={{
-        padding: "10px",
-        backgroundColor: "green",
-        color: "white",
-      }}
-    >
-      {props.name}
-    </button>
-  );
-}
-
 function App(props) {
-  <button>클릭1</button>;
-
-  return (
-    <div>
-      <MyButton name="클릭3" />
-      <br />
-      <MyButton name="클릭4" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
