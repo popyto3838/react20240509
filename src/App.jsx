bc28ea97f1bc9bc8be1702163f02b69507b0a60b
Hongbin Yoon <popyto@naver.com>
2024-05-10 16:34:06 +0900

    style 으용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+function MyButton({ name, sx, on }) {
+  return (
+    <button style={sx} onClick={on}>
+      {name}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/*  두개의 MyButton 컴포넌트를 사용 sx, name*/}
+
+      <MyButton
+        name="버튼1"
+        sx={{
+          backgroundColor: "black",
+          color: "white",
+          textDecoration: "none",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
