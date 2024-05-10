19cfd34bc27ebcc2a8684a33e461c54d56399083
Hongbin Yoon <popyto@naver.com>
2024-05-10 15:18:13 +0900

    props의 명명규칙 lowerCamelCase 예약어 쓰지 않기 class->className for->htmlFor

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-rsf;
+import React from "react";
+
+function App(props) {
+  return (
+    <div>
+      <p class="error note">lorem</p>
+      <MyComp number={3} pageNumber={1} />
+    </div>
+  );
+}
+
+export default App;
