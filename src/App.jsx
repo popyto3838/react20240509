d5af91de45040048af5c56670656f44e9f505978
Hongbin Yoon <popyto@naver.com>
2024-05-10 15:37:48 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
-function MyButton({ name, onClick }) {
-  return (
-    <div>
-      <button onClick={onClick}>{name}</button>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyButton name="샾 출력" onClick={() => console.log("####")} />
-      <MyButton name="느낌표 출력" onClick={() => console.log("!!!!")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
