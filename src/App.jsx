0bd4bd3968ad3276f35b1e1408663a74d56ead65
Hongbin Yoon <popyto@naver.com>
2024-05-10 16:18:03 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
-function MyComp() {
-  return (
-    <div>
-      <h1 style={{ color: headColor }}>lorem</h1>
-      <h1 style={{ color: bodyColor }}>lorem</h1>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp headColor={"skyblue"} bodyColor={"gold"} />
-      <MyComp headColor={"gray"} bodyColor={"linen"} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
