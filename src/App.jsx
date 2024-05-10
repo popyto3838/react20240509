1219c70b785aa365fd2072f068be990f0c307982
Hongbin Yoon <popyto@naver.com>
2024-05-10 14:38:57 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
-function MyInfo(name, team, salary, country) {
-  return (
-    <div>
-      <ul>
-        <li>이름: {name}</li>
-        <li>팀: {team}</li>
-        <li>연봉: {salary}</li>
-        <li>국가: {country}</li>
-      </ul>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyInfo name="흥민" team="토트넘" salary={500} country="korea" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
