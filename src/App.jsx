918e5b131b8efd7a0d72aa3c793e35b2dc678737
Hongbin Yoon <popyto@naver.com>
2024-05-10 10:05:01 +0900

    onClick property와 함수사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,38 @@
 import React from "react";
 
 function App(props) {
-  //function : 함수
+  //이름있는 함수
 
-  //1. 이름 있는 함수
-
-  function action1(param1, param2) {
-    // 명령문들..
-
-    return 2;
+  function action1() {
+    console.log("Action1");
   }
 
-  //2. 이름 없는 함수
+  //이름 없는 함수
 
-  const v = function (param1, param2) {
-    return 99;
+  const action2 = function () {
+    console.log("anonymous action");
   };
 
-  // 3. arrow function (화살표 함수)
-  // 3.1 파라미터가 여러개, 명령문이 여러 개
-  const a = (param1, param2, param3) => {
-    //  명령문..
-    //  명령문..
-  };
+  //arrow function
+  const action3 = () => console.log("anonymous action $$$$");
 
-  //3.2 파라미터가 없고 명령문이여러개
-  const b = () => {
-    //명령문들..  .
-  };
-
-  //3.3 파라미터가 한개, 명령문이 여러개
-
-  const c = (param1) => {
-    //명령문들...
-  };
-
-  //3.4. 명령문이 하나일 때
-  const e = () => {
-    console.log("명령문이 하나인 arrow function");
-  };
-  const f = () => console.log("중괄호 생략 가능");
-  // g랑h같음
-  const g = () => "중괄호 생략시 값이 리턴됨";
-  const h = () => {
-    return "중괄호 생략시 값이 리턴됨";
-  };
-
-  //함수실행
-  action1(null, null);
-  v(null, null);
-  a(null, null, null);
-
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={action1}>클릭!</button> <br />
+      <button onClick={action2}>클릭!</button> <br />
+      <button
+        onClick={function () {
+          console.log("anonymous action");
+        }}
+      >
+        클릭!
+      </button>{" "}
+      <br />
+      <button onClick={action3}> 클릭$</button>
+      <br />
+      <button onClick={() => console.log("arrow function%%")}> 클릭$</button>
+    </div>
+  );
 }
 
 export default App;
