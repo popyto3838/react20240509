742f5742a9a3d1eb21e6e129cc673fcbd81c9009
Hongbin Yoon <popyto@naver.com>
2024-05-10 16:17:45 +0900

    style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function MyComp() {
+  return (
+    <div>
+      <h1 style={{ color: headColor }}>lorem</h1>
+      <h1 style={{ color: bodyColor }}>lorem</h1>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp headColor={"skyblue"} bodyColor={"gold"} />
+      <MyComp headColor={"gray"} bodyColor={"linen"} />
+    </div>
+  );
 }
 
 export default App;
