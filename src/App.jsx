31c1822536c4250ca3602a6a96a101238d76c890
Hongbin Yoon <popyto@naver.com>
2024-05-10 16:21:31 +0900

    style 으용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+function MyComp(props){
+  return <div>
+    <h1 style={headStyle}>lorem</h1>
+    <h1 style={bodyStyle}>lorem</h1>
+  </div>
+}
+
 
 function App(props) {
-  return <div></div>;
+  return <div>
+    <MyComp headStyle={{padding:"5px",
+    border:"1px solid black",
+    color:"blue"}}
+            bodyStyle={{padding:"5px",
+            border:"1px solid black",
+            color:"blue"}}
+  </div>;
 }
 
 export default App;
