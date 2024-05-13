5ce15a85abe38415c414c55023c0160876f24548
Hongbin Yoon <popyto@naver.com>
2024-05-13 09:32:03 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, children }) {
-  console.log(name);
-  console.log(age);
-  console.log(children);
-
-  return (
-    <div>
-      <div>name:{name}</div>
-      <div>age:{age}</div>
-      <div>children:{children}</div>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={33}>
-        {/* content는 children prop*/}
-        <p>Lorem ipsum dolor.</p>
-      </MyComp>
-      <MyComp name={"lee"} age={22}>
-        <ul>
-          <li>Lorem.</li>
-          <li>Nostrum!</li>
-        </ul>
-      </MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
