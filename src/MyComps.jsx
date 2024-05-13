38554de92dd451a200a8e19019cf7145c3ba001d
Hongbin Yoon <popyto@naver.com>
2024-05-13 11:11:52 +0900

    default export

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,5 +1,2 @@
-const v1 = "korea";
-const v2 = 44;
-export const v3 = "seoul";
-
-export { v1, v2 };
+const val = "hello";
+export default val;
---
+++ src/MyValues.jsx
@@ -0,0 +1,6 @@
+//named export
+export const v1 = "son";
+
+//default export
+//한파일에서 하나만 가능
+export default "lee";
