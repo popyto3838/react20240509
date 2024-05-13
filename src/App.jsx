e9b27b96203e88bc56568b685adcaae96292aad7
Hongbin Yoon <popyto@naver.com>
2024-05-13 10:34:25 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { ArrowComp, MyComps, Yourcomp } from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <MyComps />
-      <Yourcomp />
-      <ArrowComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
