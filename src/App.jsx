c96959dfb864b95ef073c8fce7e007a3f9cf03c7
Hongbin Yoon <popyto@naver.com>
2024-05-13 10:18:47 +0900

    import, export 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { v1, v2 } from "./values2.jsx";
+import { c1, v2 as c2 } from "./values1.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>{v1}</div>
+      {/*korea*/}
+      <div>{v2}</div>
+      {/*seoul*/}
+      <div>{c1}</div>
+      {/*usa*/}
+      <div>{c2}</div>
+      {/*  ny*/}
+    </div>
+  );
 }
 
 export default App;
