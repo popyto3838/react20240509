d69b64bcbd21d2207bbf2bb506052a60a8a05093
Hongbin Yoon <popyto@naver.com>
2024-05-13 14:11:39 +0900

    state 연습시작완료 1증가/감소

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+function MyNumber() {
+  const [number, setNumber] = React.useState(1000);
+
+  return (
+    <div>
+      <div>{number}</div>
+      <div>
+        <button onClick={() => setNumber(number - 1)}>1씩감소</button>
+        <button onClick={() => setNumber(number + 1)}>1씩증가</button>
+      </div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
