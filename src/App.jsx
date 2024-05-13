7eba8c0634e625cf49e9277def6d4976a418f296
Hongbin Yoon <popyto@naver.com>
2024-05-13 14:34:30 +0900

    상태가 변경되면 다시 그려짐 state를 꼭 출력하는 용도로 사용하지 않아도됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+const nameList = ["손흥민", "박지성", "이강인", "차범근", "황희찬", "김민재"];
 
 function App(props) {
-  return <div></div>;
+  const [index, setName] = useState(0);
+
+  const name = nameList[index];
+
+  return (
+    <div>
+      <button onClick={() => setName(index - 1)}>이전</button>
+      <button onClick={() => setName(index + 1)}>다음</button>
+      <div>{name}</div>
+    </div>
+  );
 }
 
 export default App;
