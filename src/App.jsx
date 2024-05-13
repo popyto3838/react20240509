e716e937889d597d3555469ee3daa9d23ec59027
Hongbin Yoon <popyto@naver.com>
2024-05-13 09:13:53 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme={"blue"}>클릭1</Button>
-        <Button colorScheme={"orange"}>클릭2</Button>
-        <Button
-          sx={{
-            color: "black",
-            fontSize: "40px",
-            padding: "20px",
-            fontStyle: "revert-layer",
-          }}
-          colorScheme={"gray"}
-        >
-          클릭3
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
