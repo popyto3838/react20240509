756930e7160d1dbd3a8003d922157dcad2d069fb
Hongbin Yoon <popyto@naver.com>
2024-05-13 15:06:09 +0900

    input usestate 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  const isToolong = text.length > 10;
+
+  function handeler(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handeler} />
+      <p>{text}</p>
+      {isToolong && <p>11자 이상 입력할 수 없습니다</p>}
+    </div>
+  );
 }
 
 export default App;
