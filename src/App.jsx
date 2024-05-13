31fdafa9e80838927f0fc6aecf482baefab2210b
Hongbin Yoon <popyto@naver.com>
2024-05-13 09:29:58 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+function MyButton({ children }) {
+  return (
+    <div>
+      <button>{children}</button>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton>클릭1</MyButton>
+      <MyButton>클릭2</MyButton>
+    </div>
+  );
 }
 
 export default App;
