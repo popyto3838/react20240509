e0d05a20a16a3c1a50077806b55054c49438b94f
Hongbin Yoon <popyto@naver.com>
2024-05-13 12:29:44 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-
-function Mycomp1() {
-  const [val, setVal] = useState(0);
-
-  function updateVal() {
-    // react 는 re render 시점을 적절한 순간으로 선택함
-    setVal(val + 1);
-    setVal(val + 1);
-    setVal(val + 1);
-    console.log("val", val);
-  }
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={updateVal}>증가</button>{" "}
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <Mycomp1 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
