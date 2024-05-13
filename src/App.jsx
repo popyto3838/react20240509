af382a2262f11299f847a066532918594ac71b8e
Hongbin Yoon <popyto@naver.com>
2024-05-13 10:27:49 +0900

    코드리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
-import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
-  action1();
-  action2();
-  action3();
-  return (
-    <div>
-      <YourComp></YourComp>
-      <HerComp></HerComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
