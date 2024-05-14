a79b7ca92b77d7f1945bf1c0171569ead48a023b
Hongbin Yoon <popyto@naver.com>
2024-05-14 16:09:50 +0900

    axios 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    console.log("버튼 클릭됨");
+
+    axios.get("/path1");
+  }
+
+  function handleClick2() {
+    const qs = "city=seoul&address=gangnam";
+    axios.get("/path2?" + qs);
+  }
+
+  function handleClick3() {
+    const qs = new URLSearchParams();
+    qs.append("name", "slayer");
+    qs.append("age", "22");
+    axios.get("/path3?" + qs.toString());
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleClick1}>/path1 get 요청</button>
+      </div>
+      <div>
+        <button onClick={handleClick2}>
+          /path2 get 요청,city, address 요청파라미터 첨부
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClick3}>
+          /path3 get 요청 name,age요청 파라미터 첨부
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
