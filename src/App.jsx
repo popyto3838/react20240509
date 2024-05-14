6f0c2e469220d2964d40905d148d9bf92dc2af86
Hongbin Yoon <popyto@naver.com>
2024-05-14 11:12:40 +0900

    lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+function MyButton({ count, onClick }) {
+  return (
+    <div>
+      <button onClick={onClick}>CLICK {count}</button>
+    </div>
+  );
+}
+
+function MyBox({ count }) {
+  return <div>{count} 번 클릭됨</div>;
+}
 function App(props) {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <MyButton count={count} onClick={handleClick} />
+      <hr />
+      <MyBox count={count} />
+    </div>
+  );
 }
 
 export default App;
