b8665628de2def2d7bcf9fc3ad6cc6b8d74cd38a
Hongbin Yoon <popyto@naver.com>
2024-05-14 10:03:17 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>up</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [view1, setView1] = useState(true);
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={view1}
+        onChange={(e) => setView1(e.target.checked)}
+      />
+      {view1 && <MyBox />}
+    </div>
+  );
 }
 
 export default App;
