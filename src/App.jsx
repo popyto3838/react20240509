4ce2f6e52ceb833446710ab5319b5f328847be5d
Hongbin Yoon <popyto@naver.com>
2024-05-14 14:47:12 +0900

    useEffect 두번째 파라미터로 결정, 첫번째 파라미터를 trigger 하는 값들

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function MyComp() {
   // 두번째 파라미터가 없을 때
   // rendering 될 때 마다 실행
   useEffect(() => {
-    console.log("use effect의 함수 실행됨");
+    console.log("rendering 될 때 마다 실행");
   });
 
   // 두번째 파라미터에 빈 배열을 넣으면
@@ -21,14 +21,28 @@ function MyComp() {
     console.log("첫 렌더링 때만 실행되는 메소드");
   }, []);
 
-  // 첫번째 파라미터 (함수)의 리턴은
-  // unmount 될때 실행
-
+  // 첫번째 파라미터(함수)의 리턴은
+  // unmount 될 때 실행
   useEffect(() => {
     return () => {
       console.log("언마운트 될 때 실행 되는 메소드");
     };
   }, []);
+
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+
+  // 두번째 파라미터(배열)의 원소
+  // 어떤 값(주로 state, dependency)이 바뀔 때
+  useEffect(() => {
+    console.log("dependency text1가 바뀔 때");
+  }, [text1]);
+  useEffect(() => {
+    console.log("dependency text2가 바뀔 때");
+  }, [text2]);
+  useEffect(() => {
+    console.log("text1 또는 text2가 바뀔 때");
+  }, [text1, text2]);
 
   const [count, setCount] = useState(0);
 
@@ -36,6 +50,12 @@ function MyComp() {
     <div>
       hello comp
       <button onClick={() => setCount(count + 1)}>CLICK</button>
+      <div>
+        <input type="text" onChange={(e) => setText1(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setText2(e.target.value)} />
+      </div>
     </div>
   );
 }
