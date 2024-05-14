370af16e2002a3f2cb334e2819ddf7a7f4761bc4
Hongbin Yoon <popyto@naver.com>
2024-05-14 09:29:59 +0900

    컴포넌트는 각자 자기의 상태만 갖는다 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [target, set] = useState();
+  return (
+    <div>
+      <input type="text" onChange={(e) => set(e.target.value)} />
+      <p>{target}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+    </div>
+  );
 }
 
 export default App;
