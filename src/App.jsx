603fa6a6537463febc5b3a0540c05cc3189d26c2
Hongbin Yoon <popyto@naver.com>
2024-05-16 10:04:11 +0900

    /api/someurl3 의 get

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    // ajax get 요청
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    // ajax post 요청
+    axios.post("/api/someurl");
+  }
+
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton4() {
+    axios.post("/api/someurl2");
+  }
+
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@gmail.com");
+    axios.get(`/api/someurl3?${params}`);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get 요청</button>
+      <button onClick={handleClickButton2}>post 요청</button>
+
+      {/* /api/someurl2*/}
+      <button onClick={handleClickButton3}>get 요청2</button>
+      <button onClick={handleClickButton4}>post 요청2</button>
+
+      <button onClick={handleClickButton5}>get요청 with query string</button>
+    </div>
+  );
 }
 
 export default App;
